07dac0ed0e3611f5127a818f3eb43a3402c6691e
Shubham Sharma <shubhams1407@gmail.com>
2022-01-19 01:54:58 +0530

    Changes Saved

--- src/com/lock/me/Handling.java
+++ src/com/lock/me/Handling.java
@@ -1,107 +1,94 @@
 package com.lock.me;
 
-	import java.util.List;
-	import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java. util.Scanner;
 
-	public class Handling {
-		public static void WelcomePageInput() {
-			boolean running = true;
-			Scanner sc = new Scanner(System.in);
-			do {
-				try {
-					OptionDisplay.displayMenu();
-					int input = sc.nextInt();
+public class  Handling {
 
-					switch (input) {
-					case 1:
-						FileOption.displayAllFiles("LockME");
-						break;
-					case 2:
-						Handling.FileMenuOptions();
-						break;
-					case 3:
-						System.out.println("Program Exited.");
-						running = false;
-						sc.close();
-						System.exit(0);
-						break;
-					default:
-						System.out.println("Please select an Option.");
-					}
-				} catch (Exception e) {
-					System.out.println(e.getClass().getName());
-					WelcomePageInput();
-				} 
-			} while (running == true);
-		}
-		
-		public static void FileMenuOptions() {
-			boolean running = true;
-			Scanner sc = new Scanner(System.in);
-			do {
-				try {
-					OptionDisplay.displayFileMenuOptions();
-					FileOption.createLockMEFolderIfNotPresent("LockME");
-					
-					int input = sc.nextInt();
-					switch (input) {
-					case 1:
-						System.out.println("Enter the name of the file to add\"LockME\" folder");
-						String fileToAdd = sc.next();
-						
-						FileOption.createFile(fileToAdd, sc);
-						
-						break;
-					case 2:
-						System.out.println("Enter the name of the file to be deleted from \"LockME\" folder");
-						String fileToDelete = sc.next();
-						
-						FileOption.createLockMEFolderIfNotPresent("LockME");
-						List<String> filesToDelete = FileOption.displayFileLocations(fileToDelete, "LockME");
-						
-					String deletionPrompt = "\nSelect index of which file to delete?"
-					+ "\n(Enter 0 if you want to delete all elements)";
-						System.out.println(deletionPrompt);
-					
-						int index = sc.nextInt();
-						
-						if (index != 0) {
-							FileOption.deleteFileRecursively(filesToDelete.get(index - 1));
-						} else {
-							
-							for (String path : filesToDelete) {
-								FileOption.deleteFileRecursively(path);
-							}
-						}
-						
-
-						break;
-					case 3:
-
-						System.out.println("Enter the name of the file to be searched from \"LockME\" folder");
-						String fileName = sc.next();
-						
-						FileOption.createLockMEFolderIfNotPresent("LockME");
-						FileOption.displayFileLocations(fileName, "LockME");
-
-						
-						break;
-					case 4:
-						
-						return;
-					case 5:
-						
-						System.out.println("Program Exited.");
-						running = false;
-						sc.close();
-						System.exit(0);
-					default:
-						System.out.println("Please select an option.");
-					}
-				} catch (Exception e) {
-					System.out.println(e.getClass().getName());
-					FileMenuOptions();
-				}
-			} while (running == true);
+	File folder;
+	static String dir;
+     //Constructor
+      Handling()
+	{
+// Directory
+	  dir = System.getProperty("user.dir");
+	     folder = new File(dir + "/LockME");
+	     
+		if (!folder.exists()) {
+			folder.mkdir();
 		}
 	}
+	//Display files in ascending order
+	public void displayfiles() throws IOException {
+	   
+		File f1 = new File(folder + "/ First");
+		f1.createNewFile();
+		File f2 = new File(folder + "/Second");
+		f2.createNewFile();
+		String[] list = folder.list();
+		System.out.println("The files present in" +folder+ " are:- " );
+		Arrays.sort(list);
+         for( String s:list)
+         {
+        	System.out.println(s); 
+         }
+	}
+      //Add files to directory
+	public void addfile() throws IOException
+	{
+		System.out.println("\n Enter the file name to add:- ");
+		Scanner sc = new Scanner(System.in); 
+			String filename = sc.nextLine();
+			File path = new File(folder +"/" +filename);
+			  String[] list = folder.list();
+			  for(String list1:list)
+			  {
+				  if(filename.equalsIgnoreCase(list1))
+				  {
+					System.out.println("File cannot be created!");  
+				  }
+			  }
+			  path.createNewFile();
+		System.out.println("The file is added to the. " +folder);
+	}
+//Delete file from the directory
+	   public void deletefile()
+	   {   
+           System.out.println("Enter the name of the file to be deleted");
+          Scanner sc = new Scanner(System.in);
+           String filename = sc.next();
+           File filepath = new File(folder +"/"+filename);
+           String[] list = folder.list();
+           for (String file: list) {
+           if (filename.equals(file) && filepath.delete()) {
+                   System.out.println("File " + filename + " Deleted From " + folder);
+                   return;           
+	   }
+              
+}  
+           System.out.println("Operation Failed ! FILE NOT FOUND !");
+          
+	   }
+//Search the file
+public void searchfile()
+	    {
+	    	System.out.println("Enter The File Name to Search");
+	    	Scanner sc = new Scanner(System.in); 
+				String addfile  =  sc.next();
+	    	   File f = new File(folder + "/" +addfile);
+	    	       String[] list   =  folder.list();
+	    	       for(String s:list)
+	    	       {
+	    	    	  if(addfile.equals(s))
+	    	    	  {
+	    	    		  System.out.println("File Found  :" +addfile);
+	    	    		  return;
+	    	    	  }
+	    	       }
+	    	    System.out.println("File Not Found !!");	       
+	    }
+}
+
+
